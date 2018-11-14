efa1b46d4e8a11e02a614b2f533bcc36170bc11d
Sergey Litvinov <sergey.litvinov@neulion.com>
2018-11-14 15:00:00 +0700

    added routing module

---
+++ src/app/calc-weight/calc-weight.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalcWeightRoutingModule } from './calc-weight-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CalcWeightRoutingModule
+  ]
+})
+export class CalcWeightModule { }
